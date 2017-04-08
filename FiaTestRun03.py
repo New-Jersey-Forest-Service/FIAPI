dee7f5667f8398a905075ea60d58f5d3db5f4628
wzip <william.zipse@ecologicalforester.com>
2017-04-08 09:48:35 -0400

    Example Usage of PyEVALIDator
    FiaTestRun03.py reads state codes from NStates.csv; Requires
    PyEVALIDator dependencies

---
+++ FiaTestRun03.py
@@ -0,0 +1,43 @@
+from PyEVALIDator import fetchTable
+import csv
+
+def main():
+    i = 1
+    while (i < 4):
+        #define inputs for fetchTable
+        #fetchTable(st, yr, nm, dn, pg, r, c, of)
+        stateFile = open('NStates.csv')
+        states = csv.reader(stateFile)
+        #states = ('33','34','35')
+        st = ''
+        yr = '2015'
+        nm = ''
+        dn = ''
+        pg = 'None'
+        r = 'Forest type group'
+        c = 'Ownership group'
+        of = ''
+        if (i == 1):
+            nm = 'Area of forest land, in acres'
+            dn = 'No denominator - just produce estimate.'
+            ofprfix = 'FArea'
+        elif (i == 2):
+            nm = 'Number of growing-stock trees (at least 5 inches d.b.h.), in trees, on forest land'
+            dn = 'No denominator - just produce estimate.'
+            ofprfix = 'GsTrees'
+        else:
+            nm = 'Number of growing-stock trees (at least 5 inches d.b.h.), in trees, on forest land'
+            dn = 'Area of forest land, in acres'
+            ofprfix = 'TPA'
+        print "Let's make some tables! Run: "+str(i)
+        for state in states:
+            st = state[1]
+            #st = state
+            of = 'out'+ofprfix+st+'.html'
+            fetchTable(st, yr, nm, dn, pg, r, c, of)
+        print 'DONE!'
+        stateFile.close()
+        i += 1
+
+if __name__ == '__main__':
+    main()
