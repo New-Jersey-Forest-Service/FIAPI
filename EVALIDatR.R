73455970ced902079f93a425a893c5939136cd0e
Crghilardi <Crghilardi@yahoo.com>
2017-04-28 19:52:21 -0700

    update to working version and add FIA API GTR
    Added copy of Miles (2015) and uploaded proper working version of
    EVALIDatR

--- EVALIDatR.R
+++ EVALIDatR.R
@@ -14,42 +14,49 @@ library(rvest)
 
 fetchTable = function(st, yr, nm, dn, pg, r, c){
     BASEADDR = "http://apps.fs.fed.us/Evalidator/batcheval.jsp?"
-    RETYPE = "reptype=State"
-    LAT = "&lat=0"
-    LON = "&lon=-0"
-    RAD = "&radius=0"
-    num = paste("&snum=",nm)
-    den = paste("&sdenom=",dn)
-    stcode = paste("&wc=",st)
-    #yr = yr
-    page = paste("&pselected=",pg)
-    row = paste("&rselected=",r)
-    col = paste("&cselected=",c)
+    RETYPE = "reptype=Circle"
+    LAT = "&lat=45.0"
+    LON = "&lon=-93.0"
+    RAD = "&radius=50"
+    num = paste("&snum=",nm,sep="")
+    den = paste("&sdenom=",dn,sep="")
+    stcode = paste("&wc=",st,sep="")
+    yr = yr
+    page = paste("&pselected=",pg,sep="")
+    row = paste("&rselected=",r,sep="")
+    col = paste("&cselected=",c,sep="")
     OTHER = "&ptime=Current&rtime=Current&ctime=Current&wf=&wnum=&wnumdenom="
-    url = paste(BASEADDR,RETYPE,LAT,LON,RAD,num,den,stcode,yr,page,row,col,OTHER)
+    url = paste(BASEADDR,RETYPE,LAT,LON,RAD,num,den,stcode,yr,page,row,col,OTHER,sep="")
     url=gsub(" ","%20",url) #replace spaces with "%20"
-    return(url)
+    return(stringr::str_trim(url))
 }
 
 
-#testing
-st = "9"
-yr = "2015"
-nm = ""
-dn = ""
+#example of working URL from Miles(2015):
+# http://apps.fs.fed.us/Evalidator/batcheval.jsp?reptype=Circle&lat=45.0&lon=-93.0&radius=50&snum=Area%20of%20timberland,%20in%20acres&sdenom=No%20denominator%20-%20just%20produce%20estimate.&wc=272014,552014&pselected=None&rselected=Stand-size%20class&cselected=Ownership%20group%20-%20Major&ptime=Current&rtime=Current&ctime=Current&wf=&wnum=&wnumdenom=
+
+example = "http://apps.fs.fed.us/Evalidator/batcheval.jsp?reptype=Circle&lat=45.0&lon=-93.0&radius=50&snum=Area%20of%20timberland,%20in%20acres&sdenom=No%20denominator%20-%20just%20produce%20estimate.&wc=272014,552014&pselected=None&rselected=Stand-size%20class&cselected=Ownership%20group%20-%20Major&ptime=Current&rtime=Current&ctime=Current&wf=&wnum=&wnumdenom="
+        
+out=read_html(example) #works 
+
+#testing - replicating example from Miles(2015)
+st = "272014,552014"
+yr = ""
+nm = "Area of timberland, in acres"
+dn = "No denominator - just produce estimate."
 pg = "None"
-r = "Forest type group"
-c = "Ownership group"
-ofprfix = "TPA"
-of = paste("out",ofprfix,st,".html")
+r = "Stand-size class"
+c = "Ownership group - Major"
+
 
 testing=fetchTable(st, yr, nm, dn, pg, r, c)
 
-testing
-
-#returns "http://apps.fs.fed.us/Evalidator/batcheval.jsp?%20reptype=State%20&lat=0%20&lon=-0%20&radius=0%20&snum=%20%20&sdenom=%20%20&wc=%209%202015%20&pselected=%20None%20&rselected=%20Forest%20type%20group%20&cselected=%20Ownership%20group%20&ptime=Current&rtime=Current&ctime=Current&wf=&wnum=&wnumdenom="
+testing==example #should return TRUE
 
 #rvest syntax example from online
-webpage <- read_html(testing) #not working
+webpage <- read_html(testing)
+
+tbl=html_nodes(webpage,"table") #extract nodes
+df=html_table(tbl)[[1]] #put data into dataframe
 
 
