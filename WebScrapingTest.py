7ce5e5b67c487c44d6c91d072d2faae276aaa91d
Tim McWilliams <tmcwilliams@smu.edu>
2017-07-20 10:47:08 -0400

    Update WebScrapingTest.py

--- WebScrapingTest.py
+++ WebScrapingTest.py
@@ -13,7 +13,7 @@ page = urllib2.urlopen(table)
 from bs4 import BeautifulSoup
 
 #Parse the html in the 'page' variable & store it in bs4 format
-soup = BeautifulSoup(page, 'lxml')
+soup = BeautifulSoup(page, 'html.parser')
 
 #Print out the html code with the function prettify
 print soup.prettify()
@@ -26,11 +26,50 @@ print type(table2)
 #Create new table as a dataframe
 new_table = pd.DataFrame(columns=range(0,4))
 
-#Looping throught the tr and td fields to pull the data
-for row in table2('tr'):
-    column_marker = 0
-    columns = row.find_all('td')
-    for column in columns:
-        new_table.iat[row_marker, column_marker] = column.get_text()
-        column_marker += 1
+#Pull the info from the three tables: Estimate, Sampleing Error, and # of non-zero plots in estimates
+soup.find('table').find_all('td'),{'align':'right'}
 
+#Remove tags from HTML code
+## NOT COMPLETELY WORKING YET!
+import re
+from HTMLParser import HTMLParser
+from htmlentitydefs import entitydefs
+
+class MLStripper(HTMLParser):
+    def __init__(self):
+        self.reset()
+        self.fed = []
+        self.entityref = re.compile('&[a-zA-Z][-.a-zA-Z0-9]*[^a-zA-Z0-9]')
+
+    def handle_data(self, d):
+        self.fed.append(d)
+
+    def handle_starttag(self, tag, attrs):
+        self.fed.append(' ')
+
+    def handle_endtag(self, tag):
+        self.fed.append(' ')
+
+    def handle_entityref(self, name):
+        if entitydefs.get(name) is None:
+            m = self.entityref.match(self.rawdata.splitlines()[self.lineno - 1][self.offset:])
+            entity = m.group()
+            # semicolon is consumed, other chars are not.
+            if entity[-1] != ';':
+                entity = entity[:-1]
+            self.fed.append(entity)
+        else:
+            self.fed.append(' ')
+
+    def get_data(self):
+        self.close()  # N.B. ensure all buffered data has been processed
+        return ''.join(self.fed)
+
+
+def strip_tags(html):
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()
+
+
+print strip_tags('html &zzz; tags<p>&zzz &zz: will be&amp;replaced</p>with space. NOT this &abc')
