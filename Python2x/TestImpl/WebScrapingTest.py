c36ff60023938681660ff0476050bb43794f17b2
wzip <william.zipse@ecologicalforester.com>
2017-09-04 15:48:14 -0400

    Reorganization of the Repo by Language

---
+++ Python2x/TestImpl/WebScrapingTest.py
@@ -0,0 +1,78 @@
+#Example done on FA in Connecut
+#import necessary libraries
+import urllib2
+import pandas as pd
+
+#Specify URL
+#This is a local HTML file that is generated from PyEVALIDator.py
+#Copy and paste the local HTML file into the code below
+table = "file:///C:/Users/TMccw/Anaconda2/FiaAPI/outFArea18.html"
+
+#Query the website & return the html to the variable 'page'
+page = urllib2.urlopen(table)
+
+#import the bs4 functions to parse the data returned from the website
+from bs4 import BeautifulSoup
+
+#Parse the html in the 'page' variable & store it in bs4 format
+soup = BeautifulSoup(page, 'html.parser')
+
+#Print out the html code with the function prettify
+print soup.prettify()
+
+#Find the tables & check type
+table2 = soup.find_all('table')
+print(table2)
+print type(table2)
+
+#Create new table as a dataframe
+new_table = pd.DataFrame(columns=range(0,5))
+
+#Pull the info from the three tables: 
+#Estimate, Sampleing Error, and Number of Non-Zero Plots in Estimates
+soup.find('table').find_all('td'),{'align':'right'}
+
+#Remove tags from HTML code
+## NOT COMPLETELY WORKING YET!
+import re
+from HTMLParser import HTMLParser
+from htmlentitydefs import entitydefs
+
+class MLStripper(HTMLParser):
+    def __init__(self):
+        self.reset()
+        self.fed = []
+        self.entityref = re.compile('&[a-zA-Z][-.a-zA-Z0-9]*[^a-zA-Z0-9]')
+
+    def handle_data(self, d):
+        self.fed.append(d)
+
+    def handle_starttag(self, tag, attrs):
+        self.fed.append(' ')
+
+    def handle_endtag(self, tag):
+        self.fed.append(' ')
+
+    def handle_entityref(self, name):
+        if entitydefs.get(name) is None:
+            m = self.entityref.match(self.rawdata.splitlines()[self.lineno - 1][self.offset:])
+            entity = m.group()
+            # semicolon is consumed, other chars are not.
+            if entity[-1] != ';':
+                entity = entity[:-1]
+            self.fed.append(entity)
+        else:
+            self.fed.append(' ')
+
+    def get_data(self):
+        self.close()  # N.B. ensure all buffered data has been processed
+        return ''.join(self.fed)
+
+
+def strip_tags(html):
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()
+
+
+print strip_tags()
