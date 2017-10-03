cf0758b73fc7331ae60f84c7d49310bc4e0609fe
Tim McWilliams <tmcwilliams@smu.edu>
2017-10-03 01:11:39 -0400

    MasterScrape V1.0

--- Python2x/MasterScrape.py
+++ Python2x/MasterScrape.py
@@ -1,6 +1,6 @@
 Python 2.7.13 (v2.7.13:a06454b1afa1, Dec 17 2016, 20:53:40) [MSC v.1500 64 bit (AMD64)] on win32
 Type "copyright", "credits" or "license()" for more information.
->>> # Master Scrape 
+# Master Scrape 
 # V1.0
 # Python 2.7
 # Abi Arya & Tim McWilliams
