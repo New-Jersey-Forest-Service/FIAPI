c096878a23e1d2f5a1a4417f7f87b0b99c9e7a31
AbiArya <abhishek.arya3@yahoo.com>
2018-01-09 13:21:42 -0500

    creates bargraphs dynamically, cleaned it up, and messing with some labels to make it more readable by the user

--- Python2x/TestImpl/htmlToBarGraph.py
+++ Python2x/TestImpl/htmlToBarGraph.py
@@ -20,7 +20,9 @@ for table in soup.find_all('table'):
     title=table.b
     if(title != None):
         graph=[]
-       # print(title.string.extract())
+        tmptitle=[]
+        tmptitle.append(title.string.extract())
+        graph.append(tmptitle)
         for row in table.find_all('tr'):
             col = map(cell_text, row.find_all(re.compile('t[dh]')))
             for i in range(0,len(col)):
@@ -35,26 +37,50 @@ for table in soup.find_all('table'):
 
         output.writerow([])
         tablecounter+=1
-    tables.append(graph)
+        tables.append(graph)
 
-print(tables[0])
-xcounter = 0
-counter = 0
-title = ''
-num = [0, 1, 2, 3]
-for col in tables[0]:
-    if (counter == 0):
-        title = col[1]
-    elif (counter == 1):
-        plt.xlabel(col[0])
-    else:
-        results = map(int, col[1:])
-        plt.bar(num, results, label=col[0])
-        #print(col[1:])
 
-        num = [x + 10 for x in num]
-    counter += 1
+graphstuff=[]
+for table in tables:
+   # print table
+    xcounter = 0
+    counter = 0
+    labels=[]
+    barLabels=[]
+    barLabelPos=[]
+    graphstuff.append(plt.figure())
+    tmp = graphstuff[len(graphstuff) - 1].add_subplot(1, 1, 1)
+    for col in table:
+        if (counter == 0):
+            tmp.set_title(col[0])
 
+        elif (counter == 1):
+            tmp.set_xlabel(col[0])
+        elif(counter ==2):
+            labels=col
+           # plt.xlabel(col[0])
+        else:
+            barLabels.append(col[0])
+            results = map(float, col[1:])
+            num=[]
+            for i in range(xcounter,xcounter+len(col[1:])):
+                num.append(i)
+            barLabelPos.append(xcounter)
+            rects=tmp.bar(num, results)
+            for rect in rects:
+                height = rect.get_height()
+                tmp.text(rect.get_x() + rect.get_width() / 2., 1.05 * height,
+                        '%d' % int(height),
+                        ha='center', va='bottom',fontsize=5)
+            xcounter+=10
+        counter += 1
+    tmp.set_xticks(barLabelPos)
+    #print(barLabels)
+    #tmp.set_xticklabels(barLabels)
+    print(barLabels)
+
+# Get current size
+plt.rcParams['figure.figsize']=(20,20)
 plt.show()
 
 
