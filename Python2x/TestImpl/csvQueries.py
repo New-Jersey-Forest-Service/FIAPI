6c9f1d8dd40d569112e8dc218f5905b10ab0422c
AbiArya <abhishek.arya3@yahoo.com>
2018-03-08 15:32:29 -0500

    working on gui. added a button that lets them input html file, and outputs bargraphs

--- Python2x/TestImpl/csvQueries.py
+++ Python2x/TestImpl/csvQueries.py
@@ -3,8 +3,20 @@ import tkMessageBox
 import tkFileDialog
 import csv
 import Python2x.PyEVALIDator
+from bs4 import BeautifulSoup
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib import pyplot as plt
+import re
+import sys
 
-class GUITest04Class(Tkinter.Tk):
+def cell_text(cell):
+    return " ".join(cell.stripped_strings)
+
+
+
+
+class FirstPage(Tkinter.Tk):
     def __init__(self, parent):
         Tkinter.Tk.__init__(self, parent)
         self.parent = parent
@@ -18,20 +30,105 @@ class GUITest04Class(Tkinter.Tk):
             inf = tkFileDialog.askopenfilenames()
             if inf == '':
                 tkMessageBox.showwarning("Filename", "No file selected!")
-            #else:
+            else:
                 #tkMessageBox.showinfo("Filename", inf)
-            global filename
-            filename = inf
-            app.quit()
+                filename = inf
+
+                with open(filename[0], 'rb') as csvfile:
+                    queries = csv.reader(csvfile)
+                    counter = 0
+                    for val in queries:  # this puts one csv line into val
+                        Python2x.PyEVALIDator.fetchTable(val[0], val[1], val[2], val[3], val[4], val[5], val[6], "output" + str(counter) + ".html")
+                        counter += 1
+
+            #app.quit()
             #return inf
 
         def hello():
             tkMessageBox.showinfo("Say Hello", "Hello World")
 
+        def htmlToBargraph():
+            inf = tkFileDialog.askopenfilenames()
+            if inf == '':
+                tkMessageBox.showwarning("Filename", "No file selected!")
+            else:
+                filename = inf
+                soup = BeautifulSoup(open(filename[0]), "html.parser")
+                output = csv.writer(sys.stdout)
+                tables = []
+                tablecounter = 0
+                for table in soup.find_all('table'):
+                    # title=table.find('caption')
+                    rowcounter = 0
+                    title = table.b
+                    if (title != None):
+                        graph = []
+                        tmptitle = []
+                        tmptitle.append(title.string.extract())
+                        graph.append(tmptitle)
+                        for row in table.find_all('tr'):
+                            col = map(cell_text, row.find_all(re.compile('t[dh]')))
+                            for i in range(0, len(col)):
+                                if (col[i] == '-'):
+                                    col[i] = 0
+                                else:
+                                    col[i] = col[i].encode('ascii')
+                                    col[i] = col[i].replace(',', '')
+
+                            # print(col)
+                            graph.append(col)
+
+                        output.writerow([])
+                        tablecounter += 1
+                        tables.append(graph)
+
+                graphstuff = []
+                for table in tables:
+                    # print table
+                    xcounter = 0
+                    counter = 0
+                    labels = []
+                    barLabels = []
+                    barLabelPos = []
+                    graphstuff.append(plt.figure())
+                    tmp = graphstuff[len(graphstuff) - 1].add_subplot(1, 1, 1)
+                    for col in table:
+                        if (counter == 0):
+                            tmp.set_title(col[0])
+
+                        elif (counter == 1):
+                            tmp.set_xlabel(col[0])
+                        elif (counter == 2):
+                            labels = col
+                            # plt.xlabel(col[0])
+                        else:
+                            barLabels.append(col[0])
+                            results = map(float, col[1:])
+                            num = []
+                            for i in range(xcounter, xcounter + len(col[1:])):
+                                num.append(i)
+                            barLabelPos.append(xcounter)
+                            rects = tmp.bar(num, results)
+                            for rect in rects:
+                                height = rect.get_height()
+                                tmp.text(rect.get_x() + rect.get_width() / 2., 1.05 * height,
+                                         '%d' % int(height),
+                                         ha='center', va='bottom', fontsize=5)
+                            xcounter += 10
+                        counter += 1
+                    tmp.set_xticks(barLabelPos)
+                    # print(barLabels)
+                    # tmp.set_xticklabels(barLabels)
+                    print(barLabels)
+
+                # Get current size
+                plt.rcParams['figure.figsize'] = (20, 20)
+                plt.show()
+
         flist = ['']
-        l1 = Tkinter.Label(self, text='Hello App')
-        b1 = Tkinter.Button(self, text="Say Hello", command=hello)
-        b2 = Tkinter.Button(self, text="Open File", command=infile)
+        l1 = Tkinter.Label(self, text='PyEvalidator')
+        b1 = Tkinter.Button(self, text="Html to Bargraph", command=htmlToBargraph)
+        b2 = Tkinter.Button(self, text="State Queries", command=infile)
         l1.pack()
         b1.pack()
         b2.pack()
@@ -41,15 +138,9 @@ class GUITest04Class(Tkinter.Tk):
 
 filename = ''
 global app
-app = GUITest04Class(None)
+app = FirstPage(None)
+
 
 if __name__ == '__main__':
     app.title('Test GUI')
     app.mainloop()
-    print(filename[0])
-    with open(filename[0],'rb') as csvfile:
-        queries = csv.reader(csvfile)
-        counter =0
-        for val in queries:#this puts one csv line into val
-            Python2x.PyEVALIDator.fetchTable(val[0], val[1], val[2], val[3], val[4], val[5], val[6], "output" + str(counter) + ".html")
-            counter +=1
