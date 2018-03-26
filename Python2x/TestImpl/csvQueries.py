e7b91286fa09eb996d706de42f1458a3a27b133f
AbiArya <abhishek.arya3@yahoo.com>
2018-03-26 16:47:46 -0400

    minor fixes

--- Python2x/TestImpl/csvQueries.py
+++ Python2x/TestImpl/csvQueries.py
@@ -1,3 +1,7 @@
+import re
+import sys
+from matplotlib import pyplot as plt
+
 import Tkinter
 import tkMessageBox
 import tkFileDialog
@@ -6,24 +10,20 @@ import Python2x.PyEVALIDator
 from bs4 import BeautifulSoup
 import matplotlib
 matplotlib.use("TkAgg")
-from matplotlib import pyplot as plt
-import re
-import sys
+
 
 def cell_text(cell):
     return " ".join(cell.stripped_strings)
 
 
-
-
 class FirstPage(Tkinter.Tk):
     def __init__(self, parent):
         Tkinter.Tk.__init__(self, parent)
+        self.entry = Tkinter.Entry(self)
         self.parent = parent
         self.initialize()
 
     def initialize(self):
-        self.entry = Tkinter.Entry(self)
         self.geometry("450x300")
 
         def stateQueries():
@@ -31,21 +31,15 @@ class FirstPage(Tkinter.Tk):
             if inf == '':
                 tkMessageBox.showwarning("Filename", "No file selected!")
             else:
-                #tkMessageBox.showinfo("Filename", inf)
                 filename = inf
-
+#                dir=tkFileDialog.askdirectory()
                 with open(filename[0], 'rb') as csvfile:
                     queries = csv.reader(csvfile)
                     counter = 0
                     for val in queries:  # this puts one csv line into val
-                        Python2x.PyEVALIDator.fetchTable(val[0], val[1], val[2], val[3], val[4], val[5], val[6], "output" + str(counter) + ".html")
+                        Python2x.PyEVALIDator.fetchTable(val[0], val[1], val[2], val[3], val[4], val[5], val[6],
+                                                         "output" + str(counter) + ".html")
                         counter += 1
-
-            #app.quit()
-            #return inf
-
-        def hello():
-            tkMessageBox.showinfo("Say Hello", "Hello World")
 
         def htmlToBargraph():
             inf = tkFileDialog.askopenfilenames()
@@ -53,77 +47,75 @@ class FirstPage(Tkinter.Tk):
                 tkMessageBox.showwarning("Filename", "No file selected!")
             else:
                 filename = inf
-                soup = BeautifulSoup(open(filename[0]), "html.parser")
-                output = csv.writer(sys.stdout)
-                tables = []
-                tablecounter = 0
-                for table in soup.find_all('table'):
-                    # title=table.find('caption')
-                    rowcounter = 0
-                    title = table.b
-                    if (title != None):
-                        graph = []
-                        tmptitle = []
-                        tmptitle.append(title.string.extract())
-                        graph.append(tmptitle)
-                        for row in table.find_all('tr'):
-                            col = map(cell_text, row.find_all(re.compile('t[dh]')))
-                            for i in range(0, len(col)):
-                                if (col[i] == '-'):
-                                    col[i] = 0
-                                else:
-                                    col[i] = col[i].encode('ascii')
-                                    col[i] = col[i].replace(',', '')
+                for file in filename:
+                    soup = BeautifulSoup(open(file), "html.parser")
+                    output = csv.writer(sys.stdout)
+                    tables = []
+                    tablecounter = 0
+                    for table in soup.find_all('table'):
+                        title = table.b
+                        if (title != None):
+                            graph = []
+                            tmptitle = []
+                            tmptitle.append(title.string.extract())
+                            graph.append(tmptitle)
+                            for row in table.find_all('tr'):
+                                col = map(cell_text, row.find_all(re.compile('t[dh]')))
+                                for i in range(0, len(col)):
+                                    if (col[i] == '-'):
+                                        col[i] = 0
+                                    else:
+                                        col[i] = col[i].encode('ascii')
+                                        col[i] = col[i].replace(',', '')
 
-                            # print(col)
-                            graph.append(col)
+                                graph.append(col)
 
-                        output.writerow([])
-                        tablecounter += 1
-                        tables.append(graph)
+                            output.writerow([])
+                            tablecounter += 1
+                            tables.append(graph)
 
-                graphstuff = []
-                for table in tables:
-                    # print table
-                    xcounter = 0
-                    counter = 0
-                    labels = []
-                    barLabels = []
-                    barLabelPos = []
-                    graphstuff.append(plt.figure())
-                    tmp = graphstuff[len(graphstuff) - 1].add_subplot(1, 1, 1)
-                    for col in table:
-                        if (counter == 0):
-                            tmp.set_title(col[0])
+                    graphstuff = []
+                    for table in tables:
+                        # print table
+                        xcounter = 0
+                        counter = 0
+                        labels = []
+                        barLabels = []
+                        barLabelPos = []
+                        graphstuff.append(plt.figure())
+                        tmp = graphstuff[len(graphstuff) - 1].add_subplot(1, 1, 1)
+                        for col in table:
+                            if (counter == 0):
+                                tmp.set_title(col[0])
 
-                        elif (counter == 1):
-                            tmp.set_xlabel(col[0])
-                        elif (counter == 2):
-                            labels = col
-                            # plt.xlabel(col[0])
-                        else:
-                            barLabels.append(col[0])
-                            results = map(float, col[1:])
-                            num = []
-                            for i in range(xcounter, xcounter + len(col[1:])):
-                                num.append(i)
-                            barLabelPos.append(xcounter)
-                            rects = tmp.bar(num, results)
-                            for rect in rects:
-                                height = rect.get_height()
-                                tmp.text(rect.get_x() + rect.get_width() / 2., 1.05 * height,
-                                         '%d' % int(height),
-                                         ha='center', va='bottom', fontsize=5)
-                            xcounter += 10
-                        counter += 1
-                    tmp.set_xticks(barLabelPos)
-                    # print(barLabels)
-                    # tmp.set_xticklabels(barLabels)
-                    #print(barLabels)
+                            elif (counter == 1):
+                                tmp.set_xlabel(col[0])
+                            elif (counter == 2):
+                                labels = col
+                                # plt.xlabel(col[0])
+                            else:
+                                barLabels.append(col[0])
+                                results = map(float, col[1:])
+                                num = []
+                                for i in range(xcounter, xcounter + len(col[1:])):
+                                    num.append(i)
+                                barLabelPos.append(xcounter)
+                                rects = tmp.bar(num, results)
+                                for rect in rects:
+                                    height = rect.get_height()
+                                    tmp.text(rect.get_x() + rect.get_width() / 2., 1.05 * height,
+                                             '%d' % int(height),
+                                             ha='center', va='bottom', fontsize=5)
+                                xcounter += 10
+                            counter += 1
+                        tmp.set_xticks(barLabelPos)
+                        # print(barLabels)
+                        # tmp.set_xticklabels(barLabels)
+                        #print(barLabels)
 
-                # Get current size
-                plt.rcParams['figure.figsize'] = (20, 20)
-                plt.show()
+                    # Get current size
+                    plt.rcParams['figure.figsize'] = (20, 20)
+                    plt.show()
 
         def latLonQueries():
             inf = tkFileDialog.askopenfilenames()
@@ -132,30 +124,27 @@ class FirstPage(Tkinter.Tk):
             else:
                 # tkMessageBox.showinfo("Filename", inf)
                 filename = inf
-
-                with open(filename[0], 'rb') as csvfile:
-                    queries = csv.reader(csvfile)
-                    counter = 0
-                    for val in queries:  # this puts one csv line into val
-                        Python2x.PyEVALIDator.fetchTable(val[0], val[1], val[2], val[3], val[4], val[5], val[6], "output" + str(counter) + ".html",val[7], val[8], val[9])
-                        counter += 1
+                for file in filename:
+                    with open(file, 'rb') as csvfile:
+                        queries = csv.reader(csvfile)
+                        counter = 0
+                        for val in queries:  # this puts one csv line into val   st, yr, nm, dn, pg, r, c, of,45,-93,50
+                            Python2x.PyEVALIDator.fetchTable(val[0], val[1], val[2], val[3], val[4], val[5], val[6], 'latlonoutput' + str(counter) + '.html', val[7], val[8], val[9])
+                            counter += 1
+                        print("donezo")
 
         flist = ['']
-        l1 = Tkinter.Label(self, text='PyEvalidator')
         b1 = Tkinter.Button(self, text="Html to Bargraph", command=htmlToBargraph)
         b2 = Tkinter.Button(self, text="State Queries", command=stateQueries)
         b3 = Tkinter.Button(self, text="Lat Lon Queries", command=latLonQueries)
-        l1.pack()
         b1.place(x=150, y=140)
-        #b1.pack()
         b2.place(x=160, y=80)
-        b3.place(x=155, y = 200)
-        #b2.pack()
+        b3.place(x=155, y=200)
         if flist[0] != '':
-            print flist[0]
+            print(flist[0])
 
 
 if __name__ == '__main__':
     app = FirstPage(None)
-    app.title('Test GUI')
+    app.title('PyEvalidator')
     app.mainloop()
