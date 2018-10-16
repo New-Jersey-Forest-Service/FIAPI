6294cf57a2fb732868709146adf3d6ab9b64a6bb
Timothy McWilliams <tmcwilliams@harmelin.com>
2018-10-15 22:48:22 -0400

    Testing GUI

---
+++ Python2x/EVALIDatorGUI.py
@@ -0,0 +1,39 @@
+# EVALIDatorGUI.py
+# V 1.0
+# NJ Forest Service, 10/2018
+# Python 2.7
+
+from tkinter import *
+
+fields = 'State', 'Year', 'Numerator', 'Denominator', 'Page', 'Row', 'Column', 'Lat', 'Long', 'Radius'
+
+def fetch(entries):
+   for entry in entries:
+      field = entry[0]
+      text  = entry[1].get()
+      print('%s: "%s"' % (field, text)) 
+
+def makeform(root, fields):
+   entries = []
+   for field in fields:
+      row = Frame(root)
+      lab = Label(row, width=15, text=field, anchor='w')
+      ent = Entry(row)
+      row.pack(side=TOP, fill=X, padx=5, pady=5)
+      lab.pack(side=LEFT)
+      ent.pack(side=RIGHT, expand=YES, fill=X)
+      entries.append((field, ent))
+   return entries
+
+if __name__ == '__main__':
+   root = Tk()
+   ents = makeform(root, fields)
+   root.bind('<Return>', (lambda event, e=ents: fetch(e)))
+
+   b1 = Button(root, text='Show',
+          command=(lambda e=ents: fetch(e)))
+   b1.pack(side=LEFT, padx=5, pady=5)
+
+   b2 = Button(root, text='Quit', command=root.quit)
+   b2.pack(side=LEFT, padx=5, pady=5)
+   root.mainloop()
