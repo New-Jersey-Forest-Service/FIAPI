358369695c1f45bc478ed3517f174ffbd4672fa7
Timothy McWilliams <tmcwilliams@harmelin.com>
2018-10-18 08:17:53 -0400

    gui stuff

--- Python2x/EVALIDatorGUI.py
+++ Python2x/EVALIDatorGUI.py
@@ -7,50 +7,80 @@ from tkinter import *
 from PyEVALIDator import fetchTable
 from EVALIDatorVars import *
 
-fields = 'State', 'Year', 'Numerator', 'Denominator', 'Page', 'Row', 'Column', 'Lat', 'Long', 'Radius'
+fields = ['State', 'Year', 'Numerator', 'Denominator', 'Page', 'Row', 'Column', 'Lat', 'Long', 'Radius']
+ev = EVALIDatorVars()
+entries = []
 
-def fetch_fields(entries):
-   for entry in entries:
-      field = entry[0]
-      text  = entry[1].get()
-      print('%s: "%s"' % (field, text)) 
 
-def make_form(root, fields):
-   entries = []
-   for field in fields:
-      row = Frame(root)
-      lab = Label(row, width=15, text=field, anchor='w')
-      ent = Entry(row)
-      row.pack(side=TOP, fill=X, padx=5, pady=5)
-      lab.pack(side=LEFT)
-      ent.pack(side=RIGHT, expand=YES, fill=X)
-      entries.append((field, ent))
-   return entries
+def fetchFields(entries):
+  for entry in entries:
+    field = entry[0]
+    text  = entry[1].get()
+    print('%s: "%s"' % (field, text)) 
 
-def get_data():
- 
-    state = ev.stateDict[text[0]] + ev.get()
-    year = text[1]
-    num = ev.nmDict[text[2]] + ev.get()
-    den = ev.dmDict[text[3]] + ev.get()
-    page = text[4]
-    row = ev.rowDicttext[text[5]] + ev.get()
-    col = ev.colDicttext[text[6]] + ev.get()
-    lat = text[7]
-    lon = text[8]
-    rad = text[9]
- 
-    lbl.configure(text= res)
+def makeForm(root, fields):
+
+  for field in fields:
+    row = Frame(root)
+    lab = Label(row, width=15, text=field, anchor='w')
+    ent = Entry(row)
+    row.pack(side=TOP, fill=X, padx=5, pady=5)
+    lab.pack(side=LEFT)
+    entries = ent.pack(side=RIGHT, expand=YES, fill=X)
+    # entries.append((field, ent))
+
+    print(entries)
+   
+  return entries
+
+# def getData():
+
+#   print("you entered")
+#   print(entries.get())
+  # fetchTable(st, yr, nm, dn, pg, r, c, of, ot, lat = 0, lon =0, rad =0)
+
+
+def main():
+
+  root = Tk()
+  ents = makeForm(root, fields)
+  root.bind('<Return>', (lambda event, e=ents: fetch(e)))
+
+
+  # #define inputs for fetchTable
+  # states = ''
+  # st = ''
+  # yr = ''
+  # nm = ev.nmDict[root.bind('<Return>', (lambda event, e=ents: fetch(e)))]
+  # dn = ev.nmDict[root.bind('<Return>', (lambda event, e=ents: fetch(e)))]
+  # pg = 'None'
+  # r = ev.nmDict[root.bind('<Return>', (lambda event, e=ents: fetch(e)))]
+  # c = ev.nmDict[root.bind('<Return>', (lambda event, e=ents: fetch(e)))]
+  # of = ''
+  # ot = 'JSON'
+
+  # print("Let's make some tables!")
+
+  # for state in states:
+  #     st = ev.stateDict[state]
+  #     of = 'output'+st+'.json' 
+  #     fetchTable(st, yr, nm, dn, pg, r, c, of, ot, 0, 0, 0)
+
+  # print('DONE!')
 
 if __name__ == '__main__':
-   root = Tk()
-   ents = makeform(root, fields)
-   root.bind('<Return>', (lambda event, e=ents: fetch(e)))
+  # main()
+  root = Tk()
+  ents = makeForm(root, fields)
+  root.bind('<Return>', (lambda event, e=ents: fetchFields(e)))
 
-   b1 = Button(root, text='Show',
-          command=(lambda e=ents: fetch(e)))
-   b1.pack(side=LEFT, padx=5, pady=5)
+  b1 = Button(root, text='Get Data',
+         command=(lambda e=ents: fetchFields   (e)))
+  b1.pack(side=LEFT, padx=5, pady=5)
 
-   b2 = Button(root, text='Quit', command=root.quit)
-   b2.pack(side=LEFT, padx=5, pady=5)
-   root.mainloop()
+  b2 = Button(root, text='Quit', command=root.quit)
+  b2.pack(side=LEFT, padx=5, pady=5)
+
+  # fetch_data = Button(root, text="Retrieve", command=main)
+  # fetch_data.pack()
+  root.mainloop()
