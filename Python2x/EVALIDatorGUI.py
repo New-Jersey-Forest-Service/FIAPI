1ed9e4b28f2c928af521f7e5df05115525ca5a2d
Timothy McWilliams <tmcwilliams@harmelin.com>
2018-10-24 21:03:15 -0400

    GUI: in the making

--- Python2x/EVALIDatorGUI.py
+++ Python2x/EVALIDatorGUI.py
@@ -11,7 +11,6 @@ fields = ['State', 'Year', 'Numerator', 'Denominator', 'Page', 'Row', 'Column', 
 ev = EVALIDatorVars()
 entries = []
 
-
 def fetchFields(entries):
   for entry in entries:
     field = entry[0]
@@ -28,59 +27,43 @@ def makeForm(root, fields):
     lab.pack(side=LEFT)
     entries = ent.pack(side=RIGHT, expand=YES, fill=X)
     # entries.append((field, ent))
-
-    print(entries)
+    # print(fields)
    
   return entries
 
-# def getData():
-
-#   print("you entered")
-#   print(entries.get())
-  # fetchTable(st, yr, nm, dn, pg, r, c, of, ot, lat = 0, lon =0, rad =0)
-
-
 def main():
+    #define inputs for fetchTable
+    #fetchTable(st, yr, nm, dn, pg, r, c, of)
+    states = 
+    st = 
+    yr = 
+    nm = ev.nmDict[]
+    dn = ev.dmDict[]
+    pg = 'None'
+    r = ev.rowDict[]
+    c = ev.colDict[]
+    of = ''
+    ot = 'JSON'
 
-  root = Tk()
-  ents = makeForm(root, fields)
-  root.bind('<Return>', (lambda event, e=ents: fetch(e)))
+    print("Let's make some tables!")
+    for state in states:
+        st = ev.stateDict[state]
+        of = 'output'+st+'.json' 
+        fetchTable(st, yr, nm, dn, pg, r, c, of, ot, 0, 0, 0)
+    print('DONE!')
 
-
-  # #define inputs for fetchTable
-  # states = ''
-  # st = ''
-  # yr = ''
-  # nm = ev.nmDict[root.bind('<Return>', (lambda event, e=ents: fetch(e)))]
-  # dn = ev.nmDict[root.bind('<Return>', (lambda event, e=ents: fetch(e)))]
-  # pg = 'None'
-  # r = ev.nmDict[root.bind('<Return>', (lambda event, e=ents: fetch(e)))]
-  # c = ev.nmDict[root.bind('<Return>', (lambda event, e=ents: fetch(e)))]
-  # of = ''
-  # ot = 'JSON'
-
-  # print("Let's make some tables!")
-
-  # for state in states:
-  #     st = ev.stateDict[state]
-  #     of = 'output'+st+'.json' 
-  #     fetchTable(st, yr, nm, dn, pg, r, c, of, ot, 0, 0, 0)
-
-  # print('DONE!')
 
 if __name__ == '__main__':
-  # main()
   root = Tk()
   ents = makeForm(root, fields)
   root.bind('<Return>', (lambda event, e=ents: fetchFields(e)))
+  root.bind('<Return>', (lambda eff: main()))
 
   b1 = Button(root, text='Get Data',
-         command=(lambda e=ents: fetchFields   (e)))
+         command=(lambda:main()))
   b1.pack(side=LEFT, padx=5, pady=5)
 
   b2 = Button(root, text='Quit', command=root.quit)
   b2.pack(side=LEFT, padx=5, pady=5)
 
-  # fetch_data = Button(root, text="Retrieve", command=main)
-  # fetch_data.pack()
   root.mainloop()
